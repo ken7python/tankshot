7368b0e0241f191ab411edfab8f59e3eca4ff105
ken7python <66853112+ken7python@users.noreply.github.com>
2024-09-03 18:26:27 +0900

    update0903

--- tankshoot.c
+++ tankshoot.c
@@ -61,6 +61,7 @@ int main(void)
         }else
         if (IsKeyPressed(KEY_RIGHT)){
             tank_d = tank_d + 90;
+            /*
 
             if (tank_d == 90){
                 tank_x = tank_x + 50;
@@ -75,6 +76,10 @@ int main(void)
                 tank_y = tank_y - 50;
                 tank_d = 0;
             }
+            */
+            if (tank_d == 360){
+                tank_d = 0;
+            }
         }else
         if (IsKeyPressed(KEY_LEFT)){
             tank_d = tank_d - 90;
@@ -82,7 +87,7 @@ int main(void)
             if (tank_d == -90){
                 tank_d = 270;
             }
-
+    /*
             if (tank_d == 270){
                 tank_y = tank_y + 50;
             }else
@@ -96,6 +101,7 @@ int main(void)
                 tank_x = tank_x - 50;
                 tank_d = 0;
             }
+            */
 
         }else
         if (IsKeyPressed(KEY_SPACE) && is_bullet_on_screen==0 ){
@@ -125,13 +131,13 @@ int main(void)
 
         int x_distance = enemy_x - bullet_x;
         int y_distance = enemy_y - bullet_y;
-        if (x_distance > 0 && x_distance < 50 && y_distance > 0 && y_distance < 50 ){
+        if (x_distance > -25 && x_distance < 25 && y_distance > -25 && y_distance < 25 ){
             enemy_life = enemy_life - 1;
         }
 
         int player_enemy_x = enemy_x - tank_x;
         int player_enemy_y = enemy_y - tank_y;
-        if (enemy_life > 0 && player_enemy_x > 0 && player_enemy_x < 70 && player_enemy_y > 0 && player_enemy_y < 150 ){
+        if (enemy_life > 0 && player_enemy_x > -25 && player_enemy_x < 25 && player_enemy_y > -25 && player_enemy_y < 25 ){
             gameover = 1;
         }
 
@@ -153,10 +159,26 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawTextureEx(tank,(Vector2){tank_x,tank_y},tank_d,1.0,WHITE);
-            DrawTextureEx(bullet,(Vector2){bullet_x,bullet_y},bullet_d,1.0,WHITE);
+            //自分の表示
+            Vector2 tank_origin = {tank.width / 2.0f,tank.height / 2.0f};
+            Rectangle tank_sourceRect = {0.0f,0.0f,(float)tank.width,(float)tank.height};
+            Rectangle tank_destRect = {tank_x,tank_y,(float)tank.width,tank.height};
+            DrawTexturePro(tank, tank_sourceRect, tank_destRect, tank_origin, tank_d, WHITE);
+
+            //弾の表示
+            Vector2 bullet_origin = {bullet.width / 2.0f,bullet.height / 2.0f};
+            Rectangle bullet_sourceRect = {0.0f,0.0f,(float)bullet.width,(float)bullet.height};
+            Rectangle bullet_destRect = {bullet_x,bullet_y,(float)bullet.width,bullet.height};
+            //DrawTextureEx(bullet,(Vector2){bullet_x,bullet_y},bullet_d,1.0,WHITE);
+            DrawTexturePro(bullet, bullet_sourceRect, bullet_destRect, bullet_origin, enemy_d, WHITE);
+            //DrawTextureEx(tank,(Vector2){tank_x,tank_y},tank_d,1.0,WHITE);
             if (enemy_life > 0){
-                DrawTextureEx(enemy,(Vector2){enemy_x,enemy_y},enemy_d,1.0,WHITE);
+                //敵の表示
+                Vector2 enemy_origin = {enemy.width / 2.0f,enemy.height / 2.0f};
+                Rectangle enemy_sourceRect = {0.0f,0.0f,(float)tank.width,(float)tank.height};
+                Rectangle enemy_destRect = {enemy_x,enemy_y,(float)enemy.width,enemy.height};
+                //DrawTextureEx(enemy,(Vector2){enemy_x,enemy_y},enemy_d,1.0,WHITE);
+                DrawTexturePro(enemy, enemy_sourceRect, enemy_destRect, enemy_origin, enemy_d, WHITE);
             }
             if (gameover == 1){
                 DrawText("GAMEOVER",200,200,70,RED);
